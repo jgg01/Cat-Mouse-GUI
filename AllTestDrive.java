59852fd9de0e91b1fcd46bf58073269a03328aaf
jgg01 <jgg01@users.noreply.github.com>
2015-07-24 10:06:17 -0700

    Create AllTestDrive.java

---
+++ AllTestDrive.java
@@ -0,0 +1,13 @@
+/** All Testing. @author jgg01. */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AllTestDrive {
+
+    /* Run the unit tests in the testing files. */
+    public static void main(String... args) {
+        ucb.junit.textui.runClasses(PositionTestDrive.class);
+        ucb.junit.textui.runClasses(CatMouseTestDrive.class);
+    }
+}
