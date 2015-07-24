80455e357639d35187e2cb4f893adb68ebb6cb98
jgg01 <jgg01@users.noreply.github.com>
2015-07-24 10:11:57 -0700

    Update AllTestDrive.java

--- AllTestDrive.java
+++ AllTestDrive.java
@@ -8,6 +8,5 @@ public class AllTestDrive {
     /* Run the unit tests in the testing files. */
     public static void main(String... args) {
         ucb.junit.textui.runClasses(PositionTestDrive.class);
-        ucb.junit.textui.runClasses(CatMouseTestDrive.class);
     }
 }
